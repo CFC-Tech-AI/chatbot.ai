9c63fd13262a1ccdfb10c9c3737beb9116dfbebc
CFC-Tech-AI <pythoncfc@gmail.com>
2024-01-05 19:09:54 +0530

    first commit

--- chatbot/settings.py
+++ chatbot/settings.py
@@ -16,7 +16,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-OPENAI_API_KEY='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'
+
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
@@ -52,6 +52,7 @@ MIDDLEWARE = [
 ]
 
 ROOT_URLCONF = 'chatbot.urls'
+OPENAI_API_KEY='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'
 
 TEMPLATES = [
     {
